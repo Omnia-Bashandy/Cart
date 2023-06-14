16a30288f045a3a43828fe1851d5e66461b1725e
Omnia-Bashandy <100493759+Omnia-Bashandy@users.noreply.github.com>
2023-06-14 17:28:08 +0300

    Add project files.

---
+++ Cart/Controllers/cartController.cs
@@ -0,0 +1,95 @@
+﻿using Cart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly CartContext _db;
+
+        public CartController(CartContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/cart
+        [HttpGet]
+        public ActionResult<IEnumerable<userCart>> GetAllCarts()
+        {
+            List<userCart> carts = _db.userCarts.Include(c => c.Items).ToList();
+            return Ok(carts);
+        }
+
+        // GET: api/cart/{userId}
+        [HttpGet("{userId}")]
+        public ActionResult<userCart> GetCartByUserId(int userId)
+        {
+            userCart cart = _db.userCarts.Include(c => c.Items).FirstOrDefault(c => c.userID == userId);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            return Ok(cart);
+        }
+
+        // POST: api/cart
+        [HttpPost]
+        public ActionResult<userCart> AddCart(userCart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.userCarts.Add(cart);
+                    _db.SaveChanges();
+                    return CreatedAtAction(nameof(GetCartByUserId), new { userId = cart.userID }, cart);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+            return BadRequest();
+        }
+
+        // PUT: api/cart/{userId}
+        [HttpPut("{userId}")]
+        public ActionResult UpdateCart(int userId, [FromBody] userCart updatedCart)
+        {
+            userCart cart = _db.userCarts.FirstOrDefault(c => c.userID == userId);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            // Update the properties of the existing cart with the values from the updatedCart object
+            cart.Items = updatedCart.Items;
+
+            _db.SaveChanges();
+
+            return Ok(cart);
+        }
+
+
+        [HttpDelete("{userId}")]
+        public ActionResult DeleteCart(int userId)
+        {
+            userCart cart = _db.userCarts.Include(c => c.Items).FirstOrDefault(c => c.userID == userId);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            _db.items.RemoveRange(cart.Items);
+            _db.userCarts.Remove(cart);
+            _db.SaveChanges();
+            return Ok(cart);
+        }
+    }
+    }
---
+++ Cart/Migrations/20230614134725_newMig.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cart.Migrations
+{
+    /// <inheritdoc />
+    public partial class newMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "userCart",
+                columns: table => new
+                {
+                    userID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userCart", x => x.userID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "item",
+                columns: table => new
+                {
+                    itemId = table.Column<int>(type: "int", nullable: false),
+                    itemName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    itemDescription = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    itemPrice = table.Column<int>(type: "int", nullable: false),
+                    userCartuserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_item", x => x.itemId);
+                    table.ForeignKey(
+                        name: "FK_item_userCart_userCartuserID",
+                        column: x => x.userCartuserID,
+                        principalTable: "userCart",
+                        principalColumn: "userID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_item_userCartuserID",
+                table: "item",
+                column: "userCartuserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "item");
+
+            migrationBuilder.DropTable(
+                name: "userCart");
+        }
+    }
+}
